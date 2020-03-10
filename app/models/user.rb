e01cd25be6fbbd9933d83969dd568f55eaab48dd
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-03-10 14:42:57 +0000

    added some rspec tests for user and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,23 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+  def create
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
+     @user = User.new
+    else
+      nil
+    end
+  end
+
+  
+  validates :password, :password_confirmation, presence: true
+  validates :password, :password_confirmation, presence: true
+  validates :email, uniqueness: true
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :password, :length => { :minimum => 5}
+  validates .authenticate_with_credentials
+
+
 end
